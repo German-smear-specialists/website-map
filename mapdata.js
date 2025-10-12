2596a46736c930f808035a77eab0ed2f342c02b1
German-smear-specialists <estimates@germansmear.com>
2025-10-12 01:53:00 -0500

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,686 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "State Description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Add location markers using latitude and longitude!",
+    location_color: "#2041D4",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "/static/lib/simplemaps/map_images/",
+    fade_time: 0.1,
+    import_labels: "no",
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    "0": {},
+    "1": {},
+    "2": {},
+    "3": {},
+    "4": {},
+    "5": {},
+    "6": {},
+    "7": {},
+    "8": {},
+    "9": {},
+    "10": {},
+    "11": {},
+    "12": {},
+    "13": {},
+    "14": {},
+    "15": {},
+    HI: {
+      name: "Hawaii",
+      inactive: "Yes",
+      hide: "Yes",
+      url: "(blank)"
+    },
+    AK: {
+      name: "Alaska",
+      inactive: "Yes",
+      hide: "Yes",
+      url: "(blank)"
+    },
+    FL: {
+      name: "Florida",
+      url: "germansmear.com/german-smear-florida"
+    },
+    NH: {
+      name: "New Hampshire",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    VT: {
+      name: "Vermont",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    ME: {
+      name: "Maine",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    RI: {
+      name: "Rhode Island",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    NY: {
+      name: "New York",
+      url: "germansmear.com/german-smear-new-york"
+    },
+    PA: {
+      name: "Pennsylvania",
+      url: "germansmear.com/german-smear-pennsylvania"
+    },
+    NJ: {
+      name: "New Jersey",
+      url: "germansmear.com/german-smear-new-jersey"
+    },
+    DE: {
+      name: "Delaware",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    MD: {
+      name: "Maryland",
+      url: "germansmear.com/german-smear-maryland"
+    },
+    VA: {
+      name: "Virginia",
+      url: "germansmear.com/german-smear-virginia"
+    },
+    WV: {
+      name: "West Virginia",
+      url: "germansmear.com/german-smear-west-virginia"
+    },
+    OH: {
+      name: "Ohio",
+      url: "germansmear.com/german-smear-ohio"
+    },
+    IN: {
+      name: "Indiana",
+      url: "germansmear.com/german-smear-indiana"
+    },
+    IL: {
+      name: "Illinois",
+      url: "germansmear.com/german-smear-illinois"
+    },
+    CT: {
+      name: "Connecticut",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    WI: {
+      name: "Wisconsin",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    NC: {
+      name: "North Carolina",
+      url: "germansmear.com/german-smear-north-carolina"
+    },
+    DC: {
+      name: "District of Columbia",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    MA: {
+      name: "Massachusetts",
+      url: "germansmear.com/german-smear-massachusetts"
+    },
+    TN: {
+      name: "Tennessee",
+      url: "germansmear.com/german-smear-tennessee"
+    },
+    AR: {
+      name: "Arkansas",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    MO: {
+      name: "Missouri",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    GA: {
+      name: "Georgia",
+      url: "germansmear.com/german-smear-georgia"
+    },
+    SC: {
+      name: "South Carolina",
+      url: "germansmear.com/german-smear-south-carolina"
+    },
+    KY: {
+      name: "Kentucky",
+      url: "germansmear.com/german-smear-kentucky"
+    },
+    AL: {
+      name: "Alabama",
+      url: "germansmear.com/german-smear-alabama"
+    },
+    LA: {
+      name: "Louisiana",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    MS: {
+      name: "Mississippi",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    IA: {
+      name: "Iowa",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    MN: {
+      name: "Minnesota",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    OK: {
+      name: "Oklahoma",
+      inactive: "Yes",
+      hide: "Yes"
+    },
+    TX: {
+      name: "Texas",
+      url: "germansmear.com/german-smear-north-texas"
+    },
+    NM: {
+      name: "New Mexico",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    KS: {
+      name: "Kansas",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    NE: {
+      name: "Nebraska",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    SD: {
+      name: "South Dakota",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    ND: {
+      name: "North Dakota",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    WY: {
+      name: "Wyoming",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    MT: {
+      name: "Montana",
+      inactive: "yes",
+      hide: "yes"
+    },
+    CO: {
+      name: "Colorado",
+      url: "germansmear.com/german-smear-colorado"
+    },
+    UT: {
+      name: "Utah",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    AZ: {
+      name: "Arizona",
+      inactive: "Yes",
+      hide: "Yes"
+    },
+    NV: {
+      name: "Nevada",
+      hide: "yes",
+      inactive: "yes"
+    },
+    OR: {
+      name: "Oregon",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    WA: {
+      name: "Washington",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    CA: {
+      name: "California",
+      inactive: "Yes",
+      url: "(blank)",
+      hide: "Yes"
+    },
+    MI: {
+      name: "Michigan",
+      url: "germansmear.com/german-smear-michigan"
+    },
+    ID: {
+      name: "Idaho",
+      url: "(blank)",
+      hide: "yes"
+    },
+    GU: {
+      name: "Guam",
+      hide: "yes"
+    },
+    VI: {
+      name: "Virgin Islands",
+      hide: "yes"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "yes"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      hide: "yes"
+    },
+    AS: {
+      name: "American Samoa",
+      hide: "yes"
+    }
+  },
+  locations: {},
+  labels: {
+    NH: {
+      parent_id: "NH",
+      x: "932",
+      y: "183",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    VT: {
+      parent_id: "VT",
+      x: "883",
+      y: "243",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    RI: {
+      parent_id: "RI",
+      x: "932",
+      y: "273",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    NJ: {
+      parent_id: "NJ",
+      x: "883",
+      y: "273",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    DE: {
+      parent_id: "DE",
+      x: "883",
+      y: "303",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    MD: {
+      parent_id: "MD",
+      x: "932",
+      y: "303",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    DC: {
+      parent_id: "DC",
+      x: "884",
+      y: "332",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    MA: {
+      parent_id: "MA",
+      x: "932",
+      y: "213",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    CT: {
+      parent_id: "CT",
+      x: "932",
+      y: "243",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    HI: {
+      parent_id: "HI",
+      x: 305,
+      y: 565,
+      pill: "yes"
+    },
+    AK: {
+      parent_id: "AK",
+      x: "113",
+      y: "495"
+    },
+    FL: {
+      parent_id: "FL",
+      x: "773",
+      y: "510"
+    },
+    ME: {
+      parent_id: "ME",
+      x: "893",
+      y: "85"
+    },
+    NY: {
+      parent_id: "NY",
+      x: "815",
+      y: "158"
+    },
+    PA: {
+      parent_id: "PA",
+      x: "786",
+      y: "210"
+    },
+    VA: {
+      parent_id: "VA",
+      x: "790",
+      y: "282"
+    },
+    WV: {
+      parent_id: "WV",
+      x: "744",
+      y: "270"
+    },
+    OH: {
+      parent_id: "OH",
+      x: "700",
+      y: "240"
+    },
+    IN: {
+      parent_id: "IN",
+      x: "650",
+      y: "250"
+    },
+    IL: {
+      parent_id: "IL",
+      x: "600",
+      y: "250"
+    },
+    WI: {
+      parent_id: "WI",
+      x: "575",
+      y: "155"
+    },
+    NC: {
+      parent_id: "NC",
+      x: "784",
+      y: "326"
+    },
+    TN: {
+      parent_id: "TN",
+      x: "655",
+      y: "340"
+    },
+    AR: {
+      parent_id: "AR",
+      x: "548",
+      y: "368"
+    },
+    MO: {
+      parent_id: "MO",
+      x: "548",
+      y: "293"
+    },
+    GA: {
+      parent_id: "GA",
+      x: "718",
+      y: "405"
+    },
+    SC: {
+      parent_id: "SC",
+      x: "760",
+      y: "371"
+    },
+    KY: {
+      parent_id: "KY",
+      x: "680",
+      y: "300"
+    },
+    AL: {
+      parent_id: "AL",
+      x: "655",
+      y: "405"
+    },
+    LA: {
+      parent_id: "LA",
+      x: "550",
+      y: "435"
+    },
+    MS: {
+      parent_id: "MS",
+      x: "600",
+      y: "405"
+    },
+    IA: {
+      parent_id: "IA",
+      x: "525",
+      y: "210"
+    },
+    MN: {
+      parent_id: "MN",
+      x: "506",
+      y: "124"
+    },
+    OK: {
+      parent_id: "OK",
+      x: "460",
+      y: "360"
+    },
+    TX: {
+      parent_id: "TX",
+      x: "425",
+      y: "435"
+    },
+    NM: {
+      parent_id: "NM",
+      x: "305",
+      y: "365"
+    },
+    KS: {
+      parent_id: "KS",
+      x: "445",
+      y: "290"
+    },
+    NE: {
+      parent_id: "NE",
+      x: "420",
+      y: "225"
+    },
+    SD: {
+      parent_id: "SD",
+      x: "413",
+      y: "160"
+    },
+    ND: {
+      parent_id: "ND",
+      x: "416",
+      y: "96"
+    },
+    WY: {
+      parent_id: "WY",
+      x: "300",
+      y: "180"
+    },
+    MT: {
+      parent_id: "MT",
+      x: "280",
+      y: "95"
+    },
+    CO: {
+      parent_id: "CO",
+      x: "320",
+      y: "275"
+    },
+    UT: {
+      parent_id: "UT",
+      x: "223",
+      y: "260"
+    },
+    AZ: {
+      parent_id: "AZ",
+      x: "205",
+      y: "360"
+    },
+    NV: {
+      parent_id: "NV",
+      x: "140",
+      y: "235"
+    },
+    OR: {
+      parent_id: "OR",
+      x: "100",
+      y: "120"
+    },
+    WA: {
+      parent_id: "WA",
+      x: "130",
+      y: "55"
+    },
+    ID: {
+      parent_id: "ID",
+      x: "200",
+      y: "150"
+    },
+    CA: {
+      parent_id: "CA",
+      x: "79",
+      y: "285"
+    },
+    MI: {
+      parent_id: "MI",
+      x: "663",
+      y: "185"
+    },
+    PR: {
+      parent_id: "PR",
+      x: "620",
+      y: "545"
+    },
+    GU: {
+      parent_id: "GU",
+      x: "550",
+      y: "540"
+    },
+    VI: {
+      parent_id: "VI",
+      x: "680",
+      y: "519"
+    },
+    MP: {
+      parent_id: "MP",
+      x: "570",
+      y: "575"
+    },
+    AS: {
+      parent_id: "AS",
+      x: "665",
+      y: "580"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
